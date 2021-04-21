69e580b3d51657f5e475742c50dfe7b542963918
Atomobot <pc@atomobot.se>
2021-04-21 14:33:17 +0200

    boom boom tish

---
+++ index.js
@@ -0,0 +1,18 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stop if not functioning
+
+    key.classList.add('playing');
+    audio.currentTime = 0; //restart audio
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
